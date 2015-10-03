d230ccf193bb3d3051c1911d3346c009c6506db3
Minniezator <minniezator17@yandex.ru>
2015-10-03 11:54:31 +0500

    Fix_1 roman-time.js
    Except isNaN.

--- roman-time.js
+++ roman-time.js
@@ -1,20 +1,18 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
-function isNull(num)
+function changeIfNulle(num)
 {
-	num = num==='' ? '-':num;
+	num = num === '' ? '-' : num;
 	return num;
 }
 
 function printTime(hours, minutes)
 {
-	hours = isNull(hours);
-	minutes = isNull(minutes);
 	console.log(hours + ':' + minutes);
 }
 
-function convertTime(num)
+function convertToRoman(num)
 {
 	var units = ['', 'I', 'II','III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
 	var decade = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
@@ -23,16 +21,16 @@ function convertTime(num)
 	var fractionalPart = num%10;
 	var romanTime = decade[integerPart] + units[fractionalPart];
 	
-	return romanTime;
+	return changeIfNulle(romanTime);
 }
 
-if(hours < 0 || hours > 23 || minutes < 0 || minutes > 59 || isNaN(hours) || isNaN(minutes) || hours === undefined === minutes)
+if(hours < 0 || hours > 23 || minutes < 0 || minutes > 59 || isNaN(hours) || isNaN(minutes))
 {
-	console.log('Время указано не верно');
+	console.log('ОШИБКА! Время указано не верно.\nНа вход подается два числа, разделенных пробелом: часы и минуты.\nЧасы - число от 0 до 23.\nМинуты - число от 0 до 59.');
 }
 else
 {
-	var romanHours = convertTime(hours);
-	var romanMinutes = convertTime(minutes);
+	var romanHours = convertToRoman(hours);
+	var romanMinutes = convertToRoman(minutes);
 	printTime(romanHours, romanMinutes);
 }
